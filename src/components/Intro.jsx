6c831d80669240685b3e37a746d1341c09f1843d
HaDock404 <delescluse.g@gmail.com>
2024-10-10 19:17:35 +0200

    test intro

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import '../styles/intro.css'
+import Hadock from './Hadock'
+
+function Intro() {
+    return (
+        <div id='intro_display'>
+            <div id='intro'>
+                <Hadock/>
+            </div>
+        </div>
+    )
+}
+
+export default Intro
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Username from '../components/Username'
+import Intro from '../components/Intro'
 
 function Home() {
     return (
         <>
             <Header />
             <Username />
+            <Intro />
         </>
     )
 }
