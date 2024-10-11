6a0f78827bc71cb18f5ac06f6fcc8f2416790510
HaDock404 <delescluse.g@gmail.com>
2024-10-11 09:11:57 +0200

    introduction placement

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,14 @@
 import '../styles/intro.css'
 import Hadock from './Hadock'
+import { Link } from 'react-router-dom'
 
 function Intro() {
     return (
         <div id='intro_display'>
             <div id='intro'>
-                <Hadock/>
+                <div id='element1'><Hadock/></div>
+                <div id='element2'>Gael is an engineer, developer, and creator with 10+ years experience building products, brands, and teams. Most recently, he managed the architecture team at <Link to="https://hadock404.github.io/app-web-sodaas/" className='link'>sODAAS</Link>. He’s passionate about bridging brand and product, and crafting primitives (like icons and fonts) for the industry.</div>
+                
             </div>
         </div>
     )
