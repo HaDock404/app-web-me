cf1bec1f2f632808377b295bee78fc23b955956b
HaDock404 <delescluse.g@gmail.com>
2024-10-15 11:02:54 +0200

    test content placement

---
+++ src/components/Icon6.jsx
@@ -0,0 +1,13 @@
+function Icon6() {
+    return (
+        <svg id="icon6" width="211" height="234" viewBox="0 0 211 234" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="99.3362" y="28.6393" width="100" height="40" rx="20" transform="rotate(135 99.3362 28.6393)" fill="#FFFFFF"/>
+            <rect x="71.0519" y="141.776" width="100" height="40" rx="20" transform="rotate(-135 71.0519 141.776)" fill="#FFFFFF"/>
+            <rect x="134.5" y="91" width="126" height="40" rx="20" transform="rotate(180 134.5 91)" fill="#FFFFFF"/>
+            <path d="M113.5 51C126.238 51 138.852 53.3667 150.62 57.965C162.389 62.5633 173.082 69.3032 182.089 77.7997C191.097 86.2963 198.242 96.3832 203.116 107.485C207.991 118.586 210.5 130.484 210.5 142.5L168.302 142.5C168.302 135.711 166.885 128.989 164.13 122.717C161.376 116.445 157.34 110.747 152.251 105.946C147.162 101.146 141.121 97.3383 134.472 94.7403C127.823 92.1424 120.697 90.8053 113.5 90.8053L113.5 51Z" fill="#FFFFFF"/>
+            <rect x="168.5" y="166" width="44" height="42" rx="20.089" transform="rotate(-90 168.5 166)" fill="#FFFFFF"/>
+        </svg>
+    )
+}
+
+export default Icon6
