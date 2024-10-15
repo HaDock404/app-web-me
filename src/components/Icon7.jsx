c9074281d9d41a374c9845269b9f79f88470af7a
HaDock404 <delescluse.g@gmail.com>
2024-10-15 11:13:06 +0200

    new icon

---
+++ src/components/Icon7.jsx
@@ -0,0 +1,11 @@
+function Icon7() {
+    return (
+        <svg id="icon7" width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="95.4443" y="108.198" width="100" height="40" rx="20" transform="rotate(-90 95.4443 108.198)" fill="#1B1B1B"/>
+            <rect x="35.4443" y="8.19775" width="100" height="40" rx="20" fill="#1B1B1B"/>
+            <rect y="115.708" width="163.636" height="40" rx="20" transform="rotate(-45 0 115.708)" fill="#1B1B1B"/>
+        </svg>
+    )
+}
+
+export default Icon7
