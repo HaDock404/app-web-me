c81e18f536cf121b6b92ca982ce34aa9f0e691b3
HaDock404 <delescluse.g@gmail.com>
2024-10-19 14:20:01 +0200

    content + video

--- src/pages/Article01.jsx
+++ src/pages/Article01.jsx
@@ -32,13 +32,19 @@ function Article01() {
                 <article className='content_article_right'>
                     <div className='content_article_right_placement'>
                         <div className='content_article_right_title'>
-                            The gap
+                            The project
                         </div>
                         <div className='content_article_right_text'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus, reprehenderit dicta eius quasi harum quis beatae quibusdam! Error, corrupti cupiditate! Libero repellat illum minima iste quod saepe est amet!
+                            This project was initially just an excuse to showcase my professional experience, but I decided to push my thinking further and draw some valuable lessons from it. This reflection stems from my passion for the websites featured on the <Link to="https://www.awwwards.com/" className='content_article_right_text_link'>Awwwards</Link> platform.
                         </div>
                         <div className='content_article_right_text'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus, reprehenderit dicta eius quasi harum quis beatae quibusdam! Error, corrupti cupiditate! Libero repellat illum minima iste quod saepe est amet!
+                            I am far from having the skills of the artists whose sites grace the top spots every week, but I have taken the time to analyze them.
+                        </div>
+                        <div className='content_article_right_text'>
+                            Each of these websites guides the user through an immersive experience or tells a story, ultimately aiming to lead them to purchase a product (whatever the product may be).
+                        </div>
+                        <div className='content_article_right_text'>
+                            My goal was simple: to tell my story through a web page. This story needed to be concise, and the navigation had to be as smooth and intuitive as possible. The target user was a human resources director looking for talented profiles to recruit.
                         </div>
 
                         <div className='content_article_right_title'>
