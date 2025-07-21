2cf6dcfb91cfe5cb95f9da14282fb7ddcdce1ee9
HaDock404 <delescluse.g@gmail.com>
2025-07-21 13:18:48 +0200

    intro changed

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,12 @@ function Intro() {
         <div id='intro_display'>
             <div id='intro'>
                 <div id='element1'><Hadock/></div>
-                <div id='element2'>Gael is an engineer, developer, and creator with 10+ years experience building products, brands, and teams. Most recently, he managed the data/IA team at <Link to="https://www.erwin-labs.com/" className='link' target="_blank" rel="noopener noreferrer">Erwin-labs</Link>. He’s passionate about bridging brand and product, and coming up with new ideas (like icons and fonts) for the industry.</div>
+                <div id='element2'>
+                    {/*Gael is an engineer, developer, and creator with 10+ years experience building products, brands, and teams. Most recently, he managed the data/IA team at <Link to="https://www.erwin-labs.com/" className='link' target="_blank" rel="noopener noreferrer">Erwin-labs</Link>. He’s passionate about bridging brand and product, and coming up with new ideas (like icons and fonts) for the industry.*/}
+
+
+                    Gael is a french engineer, developer, and creator with 10+ years experience building products, brands, and teams. Most recently, he led the data/IA team at <Link to="https://www.erwin-labs.com/" className='link' target="_blank" rel="noopener noreferrer">Erwin-labs</Link>. He’s passionate about bridging brand and product, and coming up with new ideas (like AI and software) for the industry.
+                </div>
                 
             </div>
         </div>
