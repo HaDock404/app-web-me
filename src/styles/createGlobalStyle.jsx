671c4663cf016e71ef272d050b2a1a5ffddd03c1
HaDock404 <delescluse.g@gmail.com>
2025-07-30 20:18:15 +0200

    overflow updated

--- src/styles/createGlobalStyle.jsx
+++ src/styles/createGlobalStyle.jsx
@@ -2,16 +2,23 @@ import { createGlobalStyle } from "styled-components"
 
 const StyledGlobalStyle = createGlobalStyle`
     html {
-        width: 100%
+        width: 100%;
         max-width: 100%;
         overflow-x: hidden;
         overscroll-behavior: none;
     }
+    body {
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+    }
+
     ::selection {
         background-color: black;
         color: white;
     }
 `
+
 
 function GlobalStyle() {
     return <StyledGlobalStyle/>
